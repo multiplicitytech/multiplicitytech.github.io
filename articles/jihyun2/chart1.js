53990939c90655bf0c860fa7e19fcab4283c6be0
mateotis <hekfuszmate@gmail.com>
2021-04-12 14:52:54 +0400

    Created ApexCharts JS graphs for the first two figures.

---
+++ articles/jihyun2/chart1.js
@@ -0,0 +1,79 @@
+var options = {
+  series: [
+  {
+    name: "Upper income",
+    data: [29, 48]
+  },
+  {
+    name: "Middle income",
+    data: [62, 43]
+  },
+  {
+    name: "Lower income",
+    data: [10, 9]
+  }
+  ],
+  chart: {
+    width: '60%',
+    height: 350,
+    background: '#fff',
+    type: 'line',
+    dropShadow: {
+      enabled: true,
+      color: '#000',
+      top: 18,
+      left: 7,
+      blur: 10,
+      opacity: 0.2
+    },
+    toolbar: {
+      show: false
+    }
+  },
+  dataLabels: {
+    enabled: true,
+      labels: {
+        show: true,
+        formatter: function (value) {
+          return value + "%";
+        }
+      },
+  },
+  title: {
+    text: 'Rising wealth inequality in the US',
+    align: 'left'
+  },
+  grid: {
+    borderColor: '#e7e7e7',
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
+    },
+  },
+  markers: {
+    size: 1
+  },
+  xaxis: {
+    categories: ['1970', '2018'],
+  },
+  yaxis: {
+    min: 0,
+    max: 100,
+    labels: {
+      show: true,
+      formatter: function (value) {
+        return value + "%";
+      }
+    },
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'right',
+    floating: true,
+    offsetY: -25,
+    offsetX: -5
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart1"), options);
+chart.render();
---
+++ articles/jihyun2/chart2.js
@@ -0,0 +1,80 @@
+var options = {
+    series: [{
+    name: 'Options',
+    data: [59.8, 0, 0]
+  }, {
+    name: 'Non-S&P 500',
+    data: [27.3, 0, 0]
+  }, {
+    name: 'S&P 500',
+    data: [3.9, 0, 0]
+  }, {
+    name: 'Non-Marketable Limit Orders',
+    data: [0, 46.0, 0]
+  }, {
+    name: 'Marketable Limit Orders',
+    data: [0, 30.6, 0]
+  }, {
+    name: 'Market Orders',
+    data: [0, 8.3, 0]
+  }, {
+    name: 'Other Order Types',
+    data: [0, 6.0, 0]
+  }, {
+    name: 'Citadel Securities',
+    data: [0, 0, 39.5]
+  }, {
+    name: 'G1 Execution Services',
+    data: [0, 0, 25.5]
+  }, {
+    name: 'Wolverine Securities',
+    data: [0, 0, 17.1]
+  }, {
+    name: 'Virtu Americas',
+    data: [0, 0, 6.7]
+  }, {
+    name: 'Two Sigma Securities',
+    data: [0, 0, 2.2]
+  },
+  ],
+    chart: {
+    type: 'bar',
+    background: '#fff',
+    height: 350,
+    stacked: true,
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: true
+    }
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      legend: {
+        position: 'bottom',
+        offsetX: -10,
+        offsetY: 0
+      }
+    }
+  }],
+  plotOptions: {
+    bar: {
+      borderRadius: 8,
+      horizontal: false,
+    },
+  },
+  xaxis: {
+    categories: ['Product Category', 'Order Type', 'Broker Dealer'],
+  },
+  legend: {
+    show: false
+  },
+  fill: {
+    opacity: 1
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart2"), options);
+  chart.render();
