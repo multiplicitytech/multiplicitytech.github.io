f534c1b2085c4df1069b757e8d4c9b889e1bcb76
mateotis <hekfuszmate@gmail.com>
2021-04-15 21:48:08 +0400

    Remade song table natively with Tabulator, fixed subtitles.

--- articles/youssef/youssef.js
+++ articles/youssef/youssef.js
@@ -27,6 +27,40 @@ function spin(e) {
   e.currentTarget.children[0].style.transform = `rotateX(${degree}deg)`;
 }
 
+var tabledata = [
+	{title:"Dreams", artist:"Fleetwood Mac", year:1977, tiktok:620200, billboard:1},
+	{title:"You Broke Me First", artist:"Tate McRae", year:2021, tiktok:631400, billboard:17},
+	{title:"Savage Love (Laxed - Siren Beat)", artist:"Jawsh 685 x Jason Derulo", year:2020, tiktok:2500000, billboard:1},
+	{title:"Say So", artist:"Doja Cat", year:2020, tiktok:14700000, billboard:1},
+	{title:"Supalonely", artist:"BENEE", year:2019, tiktok:8000000, billboard:39},
+	{title:"The Box", artist:"Roddy Ricch", year:2019, tiktok:1800000, billboard:1},
+	{title:"Out West", artist:"JACKBOYS & Travis Scott", year:2020, tiktok:12600000, billboard:38},
+	{title:"Savage", artist:"Megan Thee Stallion", year:2020, tiktok:24700000, billboard:1},
+	{title:"Tap In", artist:"Saweetie", year:2020, tiktok:5600000, billboard:20},
+	{title:"Mood", artist:"24kGoldn & Iann Dior", year:2020, tiktok:3100000, billboard:1},
+];
+
+//initialize table
+var table = new Tabulator("#example-table", {
+    data:tabledata, //assign data to table
+    responsiveLayout: true,
+    layout:"fitColumns", //fit columns to width of table (optional)
+
+    columns:[ //Define Table Columns
+        {title:"Title", field:"title"},
+        {title:"Artist", field:"artist"},
+        {title:"Year", field:"year"},
+        {title:"TikTok Videos", field:"tiktok", formatter:"money", formatterParams:{
+			thousand:",",
+			precision:0,
+		}},
+        {title:"Billboard Hot 100 Peak Position", field:"billboard", formatter:"money", formatterParams:{
+			symbol:"#",
+			precision:0,
+		}},
+    ],
+});
+
 document.addEventListener('DOMContentLoaded', btt);
 document.addEventListener('DOMContentLoaded', playback);
 document.querySelectorAll('.cube-wrap').forEach((wrap) => {
